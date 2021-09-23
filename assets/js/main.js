15d0239319a1e663279864c5325b0bf29576d9c7
thunderheina <rheinadlon@gmail.com>
2021-09-23 13:28:16 +0700

    add show scroll up function

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,3 +47,10 @@ function scrollHeader() {
 	if (this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/* ===== SHOW SCROLL UP ===== */
+function scrollUp() {
+	const scrollUp = document.getElementById('scroll-up');
+	if (this.scrollY >= 80) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp)
